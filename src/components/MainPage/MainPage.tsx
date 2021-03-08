206fc44aa8ad8ddbd526c1bd48152d198d4d6d41
Yuliya Kryvorot <krviua@yahoo.com>
2021-03-08 14:06:37 +0300

    refactor: install material ui

--- src/components/MainPage/MainPage.tsx
+++ src/components/MainPage/MainPage.tsx
@@ -1,5 +1,19 @@
 import React from 'react';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
 
-const MainPage: React.FC = () => <h1>Main Page</h1>;
+const MainPage: React.FC = () => {
+  return (
+    <div>
+      <h1>Main Page</h1>
+      <Card>
+        <CardContent>
+          <Typography>Hello</Typography>
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
 
 export default MainPage;
