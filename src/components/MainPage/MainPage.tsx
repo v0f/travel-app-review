2ce4811c6a270f891d2fd29cdf8c49d05733fb26
Katy Rog <katkopikat@gmail.com>
2021-03-16 01:54:02 +0300

    feat: added footer and fixed layout

--- src/components/MainPage/MainPage.tsx
+++ src/components/MainPage/MainPage.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
+import MainSlider from './MainSlider';
 
 import './MainPage.css';
 
@@ -16,11 +17,16 @@ const MainPage: React.FC<Props> = (props) => {
   };
 
   return (
-    <Grid className='cards-container' container spacing={4}>
-      {React.Children.map(props.children, (countryName) => {
-        return <CountryCard id={countryName} getId={getCountryId} />;
-      })}
-    </Grid>
+    <>
+      <MainSlider />
+      <h1 className="main-heading"> Visit countries </h1>
+      <hr/>
+      <Grid className='cards-container' container spacing={4}>
+        {React.Children.map(props.children, (countryName) => {
+          return <CountryCard id={countryName} getId={getCountryId} />;
+        })}
+      </Grid>
+    </>
   );
 };
 
