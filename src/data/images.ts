ff7684db0b277117f2968cd8798dd2ac854b5dcf
Katy Rog <katkopikat@gmail.com>
2021-03-17 01:37:25 +0300

    fix: fixed responsive layout

--- src/data/images.ts
+++ src/data/images.ts
@@ -1,6 +1,4 @@
 const mainSliderImgs: Array<string> = [
-    'v1615924026/travel-app/main/1.jpg',
-    'v1615922485/travel-app/main/2.jpg',
     'v1615922715/travel-app/main/3.jpg',
     'v1615922661/travel-app/main/4.jpg',
     'v1615922551/travel-app/main/5.jpg',
@@ -8,15 +6,15 @@ const mainSliderImgs: Array<string> = [
     'v1615916147/travel-app/main/7.jpg',
     'v1615915571/travel-app/main/8.jpg',
     'v1615915402/travel-app/main/9.jpg',
-    'v1615916115/travel-app/main/11.jpg',
+    'v1615932295/travel-app/main/11.jpg',
     'v1615902423/travel-app/main/12.jpg',
-    'v1615916013/travel-app/main/13.jpg',
+    'v1615932413/travel-app/main/13.jpg',
     'v1615924127/travel-app/main/14.jpg',
-    'v1615924182/travel-app/main/15.jpg',
-    'v1615923978/travel-app/main/16.jpg',
-    'v1615923924/travel-app/main/17.jpg',
-    'v1615923870/travel-app/main/18.jpg',
-    'v1615923829/travel-app/main/19.jpg',
+    'v1615932097/travel-app/main/15.jpg',
+    'v1615932179/travel-app/main/16.jpg',
+    'v1615932533/travel-app/main/17.jpg',
+    'v1615932590/travel-app/main/18.jpg',
+    'v1615932476/travel-app/main/19.jpg',
   ];
 
   export default mainSliderImgs;
